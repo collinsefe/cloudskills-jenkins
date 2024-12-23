b0d326deb3c18ac0e9b09fc1f6f0d93e2cb2d966
Collins Orighose <collinsefe@yahoo.com>
2024-12-23 09:49:10 +0000

    changes for demo class

--- ec2.tf
+++ ec2.tf
@@ -4,17 +4,16 @@ resource "aws_default_vpc" "this" {
   }
 }
 
-resource "aws_instance" "jenkins_ec2" {
+resource "aws_instance" "jenkins" {
   ami           = "ami-0acc77abdfc7ed5a6"
-  instance_type = "t2.micro"  #var.ec2_instance_type
-  key_name      = aws_key_pair.this.key_name   #var.ec2_key_pair
+  instance_type = "t2.medium"
+  key_name      = aws_key_pair.this.key_name
 
   associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.jenkins_security_group.id]
+  vpc_security_group_ids      = [aws_security_group.jenkins.id]
   user_data                   = file("./jenkins_install.sh")
-  # user_data_replace_on_change = true
 
   tags = {
-    Name = "Jenkins-Server"
+    Name = "Jenkins Server"
   }
 }
